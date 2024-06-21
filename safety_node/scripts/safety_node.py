3ea7121fb77a1d37f34ddddd01504458025010f4
hamza-cpp <hamza.okhadir2018@gmail.com>
2024-06-21 12:01:31 +0100

    ✅ : the emergency braking node is don!

--- safety_node/scripts/safety_node.py
+++ safety_node/scripts/safety_node.py
@@ -3,8 +3,10 @@ import rclpy
 from rclpy.node import Node
 
 import numpy as np
-# TODO: include needed ROS msg type headers and libraries
+
+# include needed ROS msg type headers and libraries
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Bool
 from nav_msgs.msg import Odometry
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
@@ -13,8 +15,9 @@ class SafetyNode(Node):
     """
     The class that handles emergency braking.
     """
+
     def __init__(self):
-        super().__init__('safety_node')
+        super().__init__("safety_node")
         """
         One publisher should publish to the /drive topic with a AckermannDriveStamped drive message.
 
@@ -25,18 +28,67 @@ class SafetyNode(Node):
 
         NOTE that the x component of the linear velocity in odom is the speed
         """
-        self.speed = 0.
-        # TODO: create ROS subscribers and publishers.
+
+        self.speed = 0.0
+        # Create ROS subscribers and publishers.
+        self.scan_subscription = (
+            self.create_subscription(  # Subscribe to the scan topic
+                LaserScan, "scan", self.scan_callback, 10
+            )
+        )
+
+        self.odom_subscription = self.create_subscription(
+            Odometry,
+            "/odom",  # this is more reliable than /pf/pose/odom
+            self.odom_callback,
+            10,
+        )
+
+        # Update the speed of the car
+        self.publisher_ = self.create_publisher(AckermannDriveStamped, "drive", 1000)
+        self.teleop_publisher_ = self.create_publisher(
+            AckermannDriveStamped, "teleop", 1000
+        )
+        self.bool_publisher_ = self.create_publisher(Bool, "emergency_breaking", 1000)
 
     def odom_callback(self, odom_msg):
-        # TODO: update current speed
-        self.speed = 0.
+        # Update current speed
+        self.speed = odom_msg.twist.twist.linear.x
 
     def scan_callback(self, scan_msg):
-        # TODO: calculate TTC
-        
-        # TODO: publish command to brake
-        pass
+        # Calculate TTC
+        emergency_breaking = False
+        for idx, r in enumerate(scan_msg.ranges):
+            if np.isnan(r) or r > scan_msg.range_max or r < scan_msg.range_min:
+                continue
+            threshold = 1  # To be tuned in real vehicle
+            if (
+                r
+                / max(
+                    self.speed
+                    * np.cos(scan_msg.angle_min + idx * scan_msg.angle_increment),
+                    0.001,
+                )
+                < threshold
+            ):
+                emergency_breaking = True
+                break
+
+        emergency_msg = Bool()
+        emergency_msg.data = emergency_breaking
+
+        # Publish command to brake
+        if emergency_breaking:
+            drive_msg = AckermannDriveStamped()
+            drive_msg.drive.speed = 0.0
+            self.get_logger().info(
+                "emergency brake engaged at speed {}".format(self.speed)
+            )  # Output to Log
+            self.publisher_.publish(drive_msg)  # for autonomous control
+            self.teleop_publisher_.publish(drive_msg)  # for manual control
+
+        self.bool_publisher_.publish(emergency_msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -50,5 +102,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
